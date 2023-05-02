e25390ad60a9b55e4a3796fb23b0d6868bb16751
FrancoSurco <u202015132@upc.edu.pe>
2023-05-01 23:44:10 -0500

    feat: Prototype application
    Implement frontend base project v2

---
+++ src/public/pages/about.component.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>This is an about page</h1>
+    <pv-button @click="navigateToHome">Home</pv-button>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {}
+  },
+  methods:{
+    navigateToHome(){
+      this.$router.push({name: 'home'});
+    }
+  }
+}
+</script>
