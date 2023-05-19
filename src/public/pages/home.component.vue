731afbac83b809813bccd2afe80a5f21d3554dc3
FrancoSurco <u202015132@upc.edu.pe>
2023-05-19 00:52:55 -0500

    fix: Update home

--- src/public/pages/home.component.vue
+++ src/public/pages/home.component.vue
@@ -1,33 +1,24 @@
 <template>
   <div class="row">
-    <div class="col-md-6 right ">
-      <h1>Star your own sneaker designs right now</h1>
-      <pv-button @click="navigateToLogin" class="centered" style="background-color: #FFD400; color:black" label="Get started design" icon="pi pi-arrow-right" icon-pos="right" ></pv-button>
-      <pv-button class="centered" style="color: black" label="Trending Now" text raised ></pv-button>
+    <div class="col-md-6 d-flex flex-column justify-content-center align-items-center">
+      <h1 class="centered-title">Start your own sneaker designs right now</h1>
+      <pv-button @click="navigateToLogin" class="centered-button" style="background-color: #FFD400; color:black" label="Get started design" icon="pi pi-arrow-right" icon-pos="right"></pv-button>
+      <pv-button class="centered-button" style="color: black" label="Trending Now" text raised></pv-button>
     </div>
     <div class="col-6 right img-fluid text-center">
-<!--      Edit image-->
-      <img style="width: 100%; height: 100%" class="img-fluid" src="public/home-image.png" alt="">
+      <img class="img-fluid w-80 h-60" src="public/home-image.png" alt="">
     </div>
   </div>
 </template>
 
 <style>
-.img {
-  width: 50%;
-  height: 28.125vw; /* (50% * 9 / 16) */
-  background-size: cover;
+.centered-title {
+  text-align: center;
 }
-.centered {
+
+.centered-button {
   display: flex;
   justify-content: center;
-  text-align: center  ;
-}
-.posicion {
-
-}
-.color{
-  background-color: #FFD400;
 }
 </style>
 
