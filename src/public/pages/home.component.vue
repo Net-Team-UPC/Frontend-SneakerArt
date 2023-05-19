a40ea4837bf337c2fadedb946494c6c578ff3265
FrancoSurco <u202015132@upc.edu.pe>
2023-05-19 00:35:07 -0500

    fix: Update home

--- src/public/pages/home.component.vue
+++ src/public/pages/home.component.vue
@@ -6,6 +6,7 @@
       <pv-button class="centered" style="color: black" label="Trending Now" text raised ></pv-button>
     </div>
     <div class="col-6 right img-fluid text-center">
+<!--      Edit image-->
       <img style="width: 100%; height: 100%" class="img-fluid" src="https://s3-alpha-sig.figma.com/img/ee73/3a40/2bfcb29971dbf1324b2aee3663057040?Expires=1684108800&Signature=dvBAfiQ49VhrWhLxigmvxMJm5xGErMILHxG-rxml4MChitfvDeNy4UUNEfXmrfqWUByoZ7cE2N~q-s9t2m14-G91Jx6ME9LhGgBbx3qsvxeUL1udmmca2cbFPRNyIQMSv5mvtN5PTx354NWkpwfo7Q6wsggxwVb1GUk50leo2-TUYxBi5d7NBKuhXQZzki1QzBv7-NM34rCtvLgg6S0NuFttv46iB8DQPdTZzO8wpP6npNv7HdTpRt2NE0kYVWOxpkQJ0TaBV9L0SNGzPO8qiyIL~LgR9FLmEMi8CFZmQUm1fsLL22DGwBr8o0HLIT7bgEZNPu0NULBFPr~eu2ZEoQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" alt="">
     </div>
   </div>
