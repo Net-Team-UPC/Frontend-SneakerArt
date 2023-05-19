b0e859f4b853dacb1166d9b5ac1e44b777c86187
FrancoSurco <u202015132@upc.edu.pe>
2023-05-19 00:05:02 -0500

    fix: Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,12 @@
 import {createRouter, createWebHistory} from "vue-router";
 import HomeComponent from "../public/pages/home.component.vue";
-import ShoesListComponent from "../shoes-list/pages/shoes-list.component.vue";
 
 //Lazy loading Components
 const ShoesListService = () => import('../shoes-list/pages/shoes-list.component.vue')
 const AboutComponent = () => import('../public/pages/about.component.vue')
 const RegisterComponent = () => import("../public/pages/register.component.vue");
 const LoginComponent = () => import("../public/pages/login.component.vue");
+const ShoesListComponent = () =>import("../shoes-list/pages/shoes-list.component.vue")
 //Application Routes
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
