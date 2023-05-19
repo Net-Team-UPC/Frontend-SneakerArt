d278ae2a7cfa14714c96940dcc188a6a1cde908d
FrancoSurco <u202015132@upc.edu.pe>
2023-05-18 21:58:14 -0500

    Update: footer
    Updated footer

--- src/public/pages/footer.component.vue
+++ src/public/pages/footer.component.vue
@@ -1,7 +1,9 @@
 <template>
-  <div class="grid mt-4 p-4 align-content-start p-d-flex p-mt-auto" style="background-color: #FFD400">
-    <div class="col-12 ml-3 align-items-center justify-content-center texto" >
-      <p>Copyright &copy; 2023 SneakerArt, inc All rights reserved.”</p>
+  <div class="position">
+    <div class="grid mt-4 p-4 align-content-start p-d-flex p-mt-auto" style="background-color: #FFD400">
+      <div class="col-12 ml-3 align-items-center justify-content-center texto" >
+       <p>Copyright &copy; 2023 SneakerArt, inc All rights reserved.”</p>
+      </div>
     </div>
   </div>
 </template>
@@ -15,5 +17,22 @@ export default {
 <style>
 .texto{
   text-align:center;
+}
+body {
+  margin: 0;
+  padding: 0;
+}
+
+.container {
+  min-height: calc(100vh - 50px); /* Ajusta el valor de 50px según la altura del footer */
+  margin-bottom: 50px; /* Igual a la altura del footer */
+}
+
+.position {
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  background-color: #FFD400; /* Color de fondo del footer */
 }
 </style>
